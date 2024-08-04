374775151e03bda99d019878f99ba733488744ac
Akash Mehra <akash.mehra99@gmail.com>
2024-08-04 16:20:29 +0530

    Learing Spring Boot

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into
+    user_details (id, birth_date, name)
+values
+    (10001, current_date(), 'Akash');
+
+insert into
+    user_details (id, birth_date, name)
+values
+    (10002, current_date(), 'Divyanshi');
+
+insert into
+    user_details (id, birth_date, name)
+values
+    (10003, current_date(), 'Test');
